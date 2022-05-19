4f08aa30da32d9c2c7bce11286fe8917c387db06
FrancoisSchmitt <96196469+FrancoisSchmitt@users.noreply.github.com>
2022-05-19 16:17:44 +0200

    Update algo.js
    delete some error

--- js/algo/algo.js
+++ js/algo/algo.js
@@ -119,7 +119,7 @@ export function searchBarAlgo() {
         }
         else {
             const newResultErrorOfRecipes = document.querySelector(".all-recipes")
-            newResultErrorOfRecipes.innerHTML = `<div class="error-recipe">Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc...</div>`;;
+            newResultErrorOfRecipes.innerHTML = `<div class="error-recipe">Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc...</div>`;
         }
         newTagTabRecipes = recipeSearch;
     }
